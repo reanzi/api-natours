3438ca9645e645b75e2ce852df3a8b1cd1c72fc9
reanzi <nziku99@gmail.com>
2020-03-03 18:34:26 +0300

    Post request done

--- app.js
+++ app.js
@@ -24,4 +24,21 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {});
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body); // in the server console
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          newTour
+        }
+      });
+    }
+  );
+});
