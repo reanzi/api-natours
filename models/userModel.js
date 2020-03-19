557c1e2f5ef0aefbaf6ec23f8b54a9e03ba41436
reanzi <nziku99@gmail.com>
2020-03-19 11:58:25 +0300

    Hashing a password using bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,8 +22,34 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please Confirm your Password']
+    required: [true, 'Please Confirm your Password'],
+    validate: {
+      //This only works on Create and Save
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Password do not match!'
+    }
   }
 });
+// cost for hashing a password 10 - 15
+function salting() {
+  let value = Math.floor(Math.random() * 30);
+  if (value < 10 || value >= 15) {
+    value = 12;
+  }
+  return value;
+}
+userSchema.pre('save', async function(next) {
+  // Only run this if password was modified
+  if (!this.isModified('password')) return next();
+
+  const value = salting();
+  this.password = await bcrypt.hash(this.password, value);
+  //  Delete the passwordConfirm
+  this.passwordConfirm = undefined;
+  next();
+});
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
