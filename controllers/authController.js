1feed77315f5f0b0baf6a1cae8c40e2467e0ff5d
reanzi <nziku99@gmail.com>
2020-03-19 12:24:44 +0300

    Installed JWT and logged in user upon signup, return token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,16 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const asyncHandler = require('./../middleware/asyncHandler');
 
 exports.signup = asyncHandler(async (req, res, next) => {
   const newUser = await User.create(req.body);
-
+  //Login User
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: { user: newUser }
   });
 });
