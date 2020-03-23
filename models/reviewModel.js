b1cac2b53be20bbd3b8fbc7b1a5122483abdb3b5
reanzi <nziku99@gmail.com>
2020-03-23 16:16:22 +0300

    Modelling reviews, using parent referencing

---
+++ models/reviewModel.js
@@ -0,0 +1,41 @@
+// review,rating,createdAt, ref to tour, ref to user
+const mongoose = require('mongoose');
+// const slugify = require('slugify');
+// const User = require('./../models/userModel');
+// const validator = require('validator');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    createAt: {
+      type: Date,
+      default: Date.now()
+    },
+    author: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.']
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
