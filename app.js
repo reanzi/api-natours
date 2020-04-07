c1b270003ecc3e7b7509223dc18c4da5da70f28c
reanzi <nziku99@gmail.com>
2020-04-07 13:45:16 +0300

    Stripe intergration done and working fine, using test-api-key

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const viewRouter = require('./routes/viewRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 // const globalError = require('./controllers/errorController');
 
 // const express = require('express');
@@ -90,6 +91,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 // app.use(globalError);
 app.use(errorHandler);
