0e1527eaf557fa0c9bd3448fa7372da9b6cedcd5
reanzi <nziku99@gmail.com>
2020-04-08 09:10:17 +0300

    added start script for production

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose
     console.log('DB connection successfull!');
   });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
